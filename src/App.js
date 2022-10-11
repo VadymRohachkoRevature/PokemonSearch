a5bb439eb89e17005e76465762ee4e56ec9cea90
vadym <vadym582@revature.net>
2022-10-12 01:37:54 +0300

    Added pokemon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import pokemon from "./pokemon.json";
+import { useState } from "react";
+
+const Pokemon = ({ pokemon }) => (
+  <tr>
+    <td>{pokemon.name.english}</td>
+    <td>{pokemon.type.join(", ")}</td>
+    <td><button>Select</button></td>
+  </tr>
+);
 
 function App() {
+  const [filter, setFilter] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin: "auto", width:800, paddingTop:"1rem"}}>
+      <h1 className="title">Pokemon Search</h1>
+      <input value={filter} onChange={(evt) => setFilter(evt.target.value)} />
+      <div style={{ display: "grid", gridTemplateColumns: "70% 30%", gridColumnGap: "1rem" }}>
+        
+      <table width="100%">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pokemon
+            .filter((pokemon) => pokemon.name.english.includes(filter))
+            .slice(0, 20)
+            .map((pokemon) => (
+            <Pokemon pokemon={pokemon} key={pokemon.id} />
+          ))}
+        </tbody>
+        </table>
+        </div>
     </div>
   );
 }
